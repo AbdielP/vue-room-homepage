5dbd71d15ed0510860e7e5452ebcb4324822a9c4
AbdielP <abdiel.pinzonc@gmail.com>
2022-01-08 00:10:33 -0500

    working on 'shop now' section basic styles

--- src/components/Shopnow.vue
+++ src/components/Shopnow.vue
@@ -1,17 +1,22 @@
 <template>
-    <div></div>
+  <section>
+    <div>
+      <button>L</button>
+      <button>R</button>
+    </div>
+    <h1>Discover innovative ways to decorate</h1>
+    <p> We provide unmatched quality, comfort, and style for property owners across the country. 
+  Our experts combine form and function in bringing your vision to life. Create a room in your 
+  own style with our collection and make your property a reflection of you and what you love.</p>
+    <h3> Shop now</h3>
+  </section>
 </template>
 
 <script>
 export default {
-    name: 'Shop'
-}
+  name: "Shop",
+};
 </script>
 
 <style scoped>
-    div {
-        width: 100%;
-        height: 400px;
-        background: green;
-    }
 </style>
