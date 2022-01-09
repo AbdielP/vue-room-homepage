ae75a95e97522c544c4c063aa834cc90e06cd561
AbdielP <abdiel.pinzonc@gmail.com>
2022-01-08 21:46:08 -0500

    texts already change when gallery buttons are pressed

--- src/components/Shopnow.vue
+++ src/components/Shopnow.vue
@@ -26,8 +26,8 @@
     </div>
 
     <div class="info__container">
-      <h1 class="title">{{info[0].title}}</h1>
-      <p class="info__paragraph">{{info[0].text}}</p>
+      <h1 class="title">{{title}}</h1>
+      <p class="info__paragraph">{{text}}</p>
       <div class="shop__container">
         <p class="shop__paragraph">Shop now</p>
         <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg>
@@ -41,6 +41,9 @@ export default {
   name: "Shop",
   data() {
     return {
+      title: "Discover innovative ways to decorate",
+      text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+      currentInfo: 0,
       info: [
         { title: "Discover innovative ways to decorate", text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love." },
         { title: "We are available all across the globe", text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today." },
@@ -50,10 +53,20 @@ export default {
   },
   methods: {
     galleryBackward: function () {
-      console.log('pa atrás')
+      if (this.currentInfo > 0 && this.currentInfo <= this.info.length) {
+        this.currentInfo --;
+        this.changeInfo(this.currentInfo);
+      }
     },
-    galleryForward: () => {
-      console.log('pa lante')
+    galleryForward: function() {
+      if (this.currentInfo < this.info.length -1) {
+        this.currentInfo ++;
+        this.changeInfo(this.currentInfo);
+      }
+    },
+    changeInfo: function (value) {
+      this.title = this.info[value].title;
+      this.text = this.info[value].text;
     }
   }
 };
